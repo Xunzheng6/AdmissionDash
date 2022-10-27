a2506264bf921004bebef78499b9ed4b63e2665f
bebe <bn36@nyu.edu>
2022-10-27 17:22:57 -0400

    1st version BN

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
-st.title("Admission Dashboard")
+st.set_page_config(layout="wide")
+st.title("Find a School that Best Fits YOU")
 
-SchoolData = pd.read_excel('Student_Admissions_Dashboard_Rd1.xlsx')
+schoolData = pd.read_csv("Cohorts/Most-Recent-Cohorts-Institution.csv")
 
-st.sidebar.title('Controls')
+# Cleaning Data
+#Keeping only totals
+mask = schoolData["school_group"].isnull()
+#??schoolData = schoolData[mask]
 
+## Interface
+
+st.sidebar.title("Which Program is Right for You?")
+
+visualization=st.sidebar.radio("Select a category to narrow your path for college!",
+                 options=["School Information", "Admissions", "Student Life", "Finance"])
+
+
+sizeRange=st.sidebar.slider("Select the size of the schools:",
+                            min_value=int(schoolData["tot"].min()),
+                            max_value=int(schoolData["tot"].max()),
+                            value=(int(schoolData["tot"].min()), int(schoolData["tot"].max())))
+
+st.write(schoolData)
