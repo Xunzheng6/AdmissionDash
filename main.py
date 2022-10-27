791e96b54e8e8bfd4148650700210af9d3dd387a
Xunzheng6 <xunqing.zheng@gmail.com>
2022-10-27 08:52:35 -0400

    test commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
-import plotly_express as px
 import pandas as pd
 
 st.title("Admission Dashboard")
+
+SchoolData = pd.read_excel('Student_Admissions_Dashboard_Rd1.xlsx')
+
+st.sidebar.title('Controls')
 
